8ea7b49b74a8c614bca2db634ae3b946f2ce1a68
s-shekhar3 <60837108+s-shekhar3@users.noreply.github.com>
2026-01-30 15:04:56 +0530

    Update MyCalc.java
    Updated master to main branch

--- src/main/java/sl/calc/MyCalc.java
+++ src/main/java/sl/calc/MyCalc.java
@@ -27,7 +27,7 @@ public class MyCalc {
 	}
 	public void greeting()
 	{
-		System.out.println("Hello Jenkins from Master Branch");
+		System.out.println("Hello Jenkins from Main Branch");
 	}
 
 	public static void main(String[] args) {
